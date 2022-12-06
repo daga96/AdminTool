330b3593d6decf016182d0cbee708a167bdc5ce2
Lukanko Dagmara <dagmaraluk@purpleocean.net>
2022-12-06 12:37:05 +0900

    feat: added profile and users page

---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import { Heading, Avatar, Box, Center, Text } from "@chakra-ui/react";
+
+export default function Profile({ name, tag, text, photo }) {
+  return (
+    <Center py={6}>
+      <Box maxW="320px" w="full" borderRadius="15px" p={6} textAlign="center">
+        <Avatar
+          size={"xl"}
+          src={photo}
+          alt={"Avatar Alt"}
+          mb={4}
+          backgroundColor="#C8C8C8"
+          pos={"relative"}
+        />
+        <Heading fontSize={"2xl"} color="#FFFFFF">
+          User Name
+        </Heading>
+        <Text fontWeight={600} color="#C8C8C8" mb={4}>
+          @xxdragonxx
+        </Text>
+        <Text textAlign={"center"} color="#C8C8C8" px={3}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquam,
+          ligula sit amet hendrerit auctor, augue ante consequat diam, vitae
+          pretium urna ligula quis nunc. Nunc tincidunt consectetur mi, id
+          tempus arcu sollicitudin a.
+        </Text>
+      </Box>
+    </Center>
+  );
+}
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -7,10 +7,18 @@ import {
   GridItem,
   Icon,
 } from "@chakra-ui/react";
+import Profile from "../components/Profile";
 
 import Sidebar from "../components/sidebar/Sidebar";
+import TableBox from "../components/table/Table";
 
 export default function Users() {
+  const columnsUsers = [
+    { Header: "usrID", accessor: "usr_id" },
+    { Header: "nickname", accessor: "nickname" },
+    { Header: "date", accessor: "sign_date" },
+  ];
+
   return (
     <Flex flexDir="row" width="100vw" height="100vh" backgroundColor="#151319">
       <Sidebar />
@@ -21,6 +29,35 @@ export default function Users() {
             Users
           </Heading>
         </Box>
+        <Flex flexDir="row">
+          <Flex
+            borderRadius={15}
+            backgroundColor="#232429"
+            p={4}
+            w="50%"
+            mr={4}
+          >
+            <TableBox
+              columns={columnsUsers}
+              data={[
+                {
+                  usr_id: "U998272",
+                  nickname: "xxdragonxx",
+                  sign_date: "2022-12-06 16:22:00",
+                },
+              ]}
+            />
+          </Flex>
+          <Flex
+            borderRadius={15}
+            backgroundColor="#232429"
+            p={4}
+            w="25%"
+            justifyContent="center"
+          >
+            <Profile />
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   );
